1431afede46c0d73bf7d22db9bd8a47069ce4564
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2024-02-25 02:50:57 +0000

    complete solution?

--- runit.sh
+++ runit.sh
@@ -2,7 +2,7 @@
 
 rm -f a.out
 
-echo "Compiling: g++ *.cpp -lgtest -lgtest_main -pthread"
+echo "Compiling: g++ *.cpp -g -lgtest -lgtest_main -pthread"
 g++ *.cpp -lgtest -lgtest_main -pthread
 
 echo "Executing: ./a.out"
